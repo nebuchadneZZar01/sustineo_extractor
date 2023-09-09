3ec31db0074ff4e25a5a886a0aa871eb4d1c1c22
nebuchadneZZar01 <mykael98@live.it>
2023-09-09 19:16:59 +0200

    fix: added cli paragraph removal argument from last commit

--- sustineo_pdf2plot.py
+++ sustineo_pdf2plot.py
@@ -4,9 +4,9 @@ import argparse
 from lib.pdf2plot.plot_extractor import PDFToImage
 from lib.pdf2plot.tables_extractor import TableToCSV
 
-def main(pdf_path, language, user_correction, debug, size_factor):
+def main(pdf_path, language, user_correction, paragraph, debug, size_factor):
     print(f'File {pdf_path} selected')
-    plot_extr = PDFToImage(pdf_path, language, user_correction, debug, size_factor)
+    plot_extr = PDFToImage(pdf_path, language, user_correction, paragraph, debug, size_factor)
     plot_extr.run()
     table_extr = TableToCSV(pdf_path)
     table_extr.run()
@@ -34,10 +34,16 @@ def main(pdf_path, language, user_correction, debug, size_factor):
     if os.path.isdir(out_other_table_path):
         cnt_other_tables = len(os.listdir(out_other_table_path))
 
+    print(f'{os.path.basename(pdf_path)} extraction report:')
     print(f'{cnt_plots} plots were extracted in {out_plot_path}')
     print(f'{cnt_matrices} materiality matrices were extracted in {out_matrix_path}')
     print(f'{cnt_gri_tables} gri tables were extracted in {out_gri_table_path}')
-    print(f'{cnt_other_tables} other tables were extracted in {out_other_table_path}\n')
+    print(f'{cnt_other_tables} other tables were extracted in {out_other_table_path}')
+    
+    if not paragraph:
+        print(f'{plot_extr.paragraph_pages} plots require user intervention as were detected likely paragraphs; please run the script in paragraph removal mode using the --paragraph [-p] argument\n')
+    else:
+        print('\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='sustineo_pdf2plot',
@@ -54,9 +60,12 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--correction', action='store_true',\
                         help='enable user correction')
     
+    parser.add_argument('-p', '--paragraph', action='store_true',\
+                        help='enable automatic paragraph removal (new images will be saved in <file>/img/plot/no_par)')
+    
     parser.add_argument('-d', '--debug-mode', action='store_true',\
                         help='enable the visualization of the various passes')
-
+    
     parser.add_argument('-s', '--size-factor', type=float, default=3,\
                         help='if used in debug mode, the image sizes will be divided by the choosen\
                             scale factor for a better visualization on lower resolution screens (default=3)')
@@ -65,7 +74,7 @@ if __name__ == '__main__':
 
     if os.path.isfile(args.pathname):
         if args.pathname.endswith('pdf'):
-            main(args.pathname, args.language, args.correction, args.debug_mode, args.size_factor)
+            main(args.pathname, args.language, args.correction, args.paragraph, args.debug_mode, args.size_factor)
         else:
             print(f'{args.pathname} is not a PDF file\nPlease choose a PDF file')
             exit()
