1180b62a989f3ebd286e87f59ae0c037b21e1838
nebuchadneZZar01 <mykael98@live.it>
2023-09-20 16:00:15 +0200

    feat: added graceful exit and complexive report in multi-file extraction

--- sustineo_pdf2plot.py
+++ sustineo_pdf2plot.py
@@ -1,23 +1,55 @@
 import os
+import signal
 import argparse
 
 from lib.pdf2plot.plot_extractor import PDFToImage
 from lib.pdf2plot.tables_extractor import TableToCSV
+
+total_plot_w_ex = 0
+total_plot_ex = 0
+total_matrix_ex = 0
+total_table_other_ex = 0
+total_table_gri_ex = 0
+
+def handler(signum, frame):
+    res = input("Ctrl-C was pressed. Do you really want to exit? [Y/n] ")
+    
+    if res[0].lower() == 'y':
+        print('Gracefully exiting the program...')
+
+        print('Complexive extraction report:')
+        print(f'- {total_matrix_ex} materiality matrix have been extracted;')
+        print(f'- {total_plot_ex} plots have been extracted ({total_plot_w_ex} of which could require user intervention);')
+        print(f'- {total_table_gri_ex} gri tables have been extracted;')
+        print(f'- {total_table_other_ex} other tables have been extracted.')
+        exit(0)
+
+signal.signal(signal.SIGINT, handler)
 
 def main(pdf_path, language, headless, user_correction, paragraph, dataset_creation, debug, size_factor):
     print(f'File {pdf_path} selected')
     plot_extr = PDFToImage(pdf_path, language, headless, user_correction, paragraph, dataset_creation, debug, size_factor)
     plot_extr.run()
 
+    # complexive plot extraction stats
+    total_plot_ex += plot_extr.ex_plot_cnt
+    total_plot_w_ex += plot_extr.ex_plot_w_cnt
+    total_matrix_ex += plot_extr.ex_materiality_mat_cnt
+
     if not dataset_creation:
         table_extr = TableToCSV(pdf_path)
         table_extr.run()
+
+        # complexive table extraction stats
+        total_table_gri_ex += table_extr.ex_table_gri_cnt
+        total_table_other_ex += table_extr.ex_table_other_cnt
 
     print(f'{os.path.basename(pdf_path)} extraction report:')
     plot_extr.get_stats()
 
     if not dataset_creation:
         table_extr.get_stats()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='sustineo_pdf2plot',
@@ -52,9 +84,12 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
+
     if os.path.isfile(args.pathname):
         if args.pathname.endswith('pdf'):
-            main(args.pathname, args.language, args.headless, args.correction, args.paragraph, args.dataset_creation, args.debug_mode, args.size_factor)
+            main(args.pathname, args.language, 
+                 args.headless, args.correction, args.paragraph, 
+                 args.dataset_creation, args.debug_mode, args.size_factor)
         else:
             print(f'{args.pathname} is not a PDF file\nPlease choose a PDF file')
             exit()
@@ -64,7 +99,14 @@ if __name__ == '__main__':
             for i, fn in enumerate(os.listdir(args.pathname)):
                 complete_fn = os.path.join(args.pathname, fn)
                 print(f'Extracting data from file {i+1} of {n_files}...\n')
-                main(complete_fn, args.language, args.headless, args.correction, args.paragraph, args.dataset_creation, args.debug_mode, args.size_factor)
-                os.remove(complete_fn)
+                main(complete_fn, args.language, 
+                     args.headless, args.correction, args.paragraph, 
+                     args.dataset_creation, args.debug_mode, args.size_factor)
+                
+            print('Complexive extraction report:')
+            print(f'- {total_matrix_ex} materiality matrix have been extracted;')
+            print(f'- {total_plot_ex} plots have been extracted ({total_plot_w_ex} of which could require user intervention);')
+            print(f'- {total_table_gri_ex} gri tables have been extracted;')
+            print(f'- {total_table_other_ex} other tables have been extracted.')
         else:
             print(f'ERROR: File {args.pathname} does not exist')
